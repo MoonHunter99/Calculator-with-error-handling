57ad0e2846326a9687e74085208648ca39ecd1bd
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-12 21:07:59 +0800

    PseudoCode

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+#Ask user on what Operation to do
+#define addition operation
+#ask user for the first number
+#ask the user for seconnd number
+#try to run the numbers in the code if the type is correct
+#
+#define subtraction operation
+#ask user for the first number
+#ask the user for seconnd number
+#try to run the numbers in the code if the type is correct
+#
+#define multiplication operation
+#ask user for the first number
+#ask the user for seconnd number
+#try to run the numbers in the code if the type is correct
+#
+#define division operation
+#ask user for the first number
+#ask the user for seconnd number
+#try to run the numbers in the code if the type is correct and if it will be a zero division error
+#
+#
