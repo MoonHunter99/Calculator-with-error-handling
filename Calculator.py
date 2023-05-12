89ca92a4025c302c715da0417f5212e68b2a8a2b
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-12 21:24:18 +0800

    subtraction is now working

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,29 @@
 #Ask user on what Operation to do
 #define addition operation
-
-#ask user for the first number
-number_1_input = float(input("What is the first number? "))
-#ask the user for seconnd number
-number_2_input = float(input("What id your second number? "))
-#try to run the numbers in the code if the type is correct
-try:
-    number_output = number_1_input + number_2_input
-    print(str(number_output))
-except ValueError:
-    print("That is a string please input an integer")
+def add():
+    #ask user for the first number
+    number_1_input = float(input("What is the first number? "))
+    #ask the user for seconnd number
+    number_2_input = float(input("What id your second number? "))
+    #try to run the numbers in the code if the type is correct
+    try:
+        number_output = number_1_input + number_2_input
+        print(str(number_output))
+    except ValueError:
+        print("That is a string please input an integer")
 #
 #define subtraction operation
+def subtract():
     #ask user for the first number
+    number_1_input = float(input("What is yur first number? "))
     #ask the user for seconnd number
+    number_2_input = float(input("What is yur second number? "))
     #try to run the numbers in the code if the type is correct
+    try:
+        number_output = number_1_input - number_2_input
+        print(str(number_output))
+    except ValueError:
+        print("That is a string please input an integer")
 #
 #define multiplication operation
     #ask user for the first number
