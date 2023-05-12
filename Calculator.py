07d121deb498afca786aa277d946c12f0de1569f
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-12 21:31:37 +0800

    Multiplication working

--- Calculator.py
+++ Calculator.py
@@ -26,9 +26,17 @@ def subtract():
         print("That is a string please input an integer")
 #
 #define multiplication operation
+def multiply():
     #ask user for the first number
+    number_1_input = float(input("What is yur first number? "))
     #ask the user for seconnd number
+    number_2_input = float(input("What is yur second number? "))
     #try to run the numbers in the code if the type is correct
+    try:
+        number_output = number_1_input * number_2_input
+        print(str(number_output))
+    except ValueError:
+        print("That is a string please input an integer")
 #
 #define division operation
     #ask user for the first number
