d20e14efb996bcf4ce879034d1a48c6ee2606362
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-12 21:18:07 +0800

    Addition is working

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,30 @@
 #Ask user on what Operation to do
 #define addition operation
+
 #ask user for the first number
+number_1_input = float(input("What is the first number? "))
 #ask the user for seconnd number
+number_2_input = float(input("What id your second number? "))
 #try to run the numbers in the code if the type is correct
+try:
+    number_output = number_1_input + number_2_input
+    print(str(number_output))
+except ValueError:
+    print("That is a string please input an integer")
 #
 #define subtraction operation
-#ask user for the first number
-#ask the user for seconnd number
-#try to run the numbers in the code if the type is correct
+    #ask user for the first number
+    #ask the user for seconnd number
+    #try to run the numbers in the code if the type is correct
 #
 #define multiplication operation
-#ask user for the first number
-#ask the user for seconnd number
-#try to run the numbers in the code if the type is correct
+    #ask user for the first number
+    #ask the user for seconnd number
+    #try to run the numbers in the code if the type is correct
 #
 #define division operation
-#ask user for the first number
-#ask the user for seconnd number
-#try to run the numbers in the code if the type is correct and if it will be a zero division error
+    #ask user for the first number
+    #ask the user for seconnd number
+    #try to run the numbers in the code if the type is correct and if it will be a zero division error
 #
 #
