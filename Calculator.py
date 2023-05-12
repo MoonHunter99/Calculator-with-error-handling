e3adcc37a5ac57952be612e6279219b6d0e17a74
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-12 21:40:20 +0800

    Division is now working

--- Calculator.py
+++ Calculator.py
@@ -39,8 +39,18 @@ def multiply():
         print("That is a string please input an integer")
 #
 #define division operation
+def divide():
     #ask user for the first number
+    number_1_input = float(input("What is yur first number? "))
     #ask the user for seconnd number
+    number_2_input = float(input("What is yur second number? "))
     #try to run the numbers in the code if the type is correct and if it will be a zero division error
+    try:
+        number_output = number_1_input / number_2_input
+        print(str(number_output))
+    except ValueError:
+        print("That is a string please input an integer")
+    except ZeroDivisionError:
+        print("The number is being divided by a zero")
 #
 #
